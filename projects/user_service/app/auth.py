0d922e9e5292124bd93984b407142d9aa13cd9b3
GGWPVV <gvlportugal95@gmail.com>
2025-07-15 23:45:18 +0100

    fix: getting  token

--- projects/user_service/app/auth.py
+++ projects/user_service/app/auth.py
@@ -1,5 +1,5 @@
-import jwt
-from datetime import datetime, timedelta
+import jwt as PyJWT
+from datetime import datetime, timedelta, timezone
 
 SECRET_KEY = "mysecret"
 ALGORITHM = "HS256"
@@ -8,7 +8,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 60
 def create_jwt(user_id):
     payload = {
         "sub": str(user_id),
-        "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        "exp": datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     }
-    token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    token = PyJWT.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
     return token
