1722b17a349fdee50199f1515aa1c5fc44f9d05e
GGWPVV <gvlportugal95@gmail.com>
2025-07-15 13:23:31 +0100

    fix: error message for db connection

--- projects/score_service/app/database.py
+++ projects/score_service/app/database.py
@@ -1,8 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.exc import OperationalError
 
 SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://scorepostgres:scorepassword@scoredb:5432/scoredb"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
+
+# Проверка подключения к базе
+try:
+    with engine.connect() as connection:
+        print("Successful connection to scoredb.")
+except OperationalError as e:
+    print(f"Connection error: {e}")
+    raise
--- projects/user_service/app/database.py
+++ projects/user_service/app/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.exc import OperationalError
 
 SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://userpostgres:userpassword@userdb:5432/userdb"
 
@@ -7,3 +8,11 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+# Проверка подключения к базе
+try:
+    with engine.connect() as connection:
+        print(" Successful connection to userdb.")
+except OperationalError as e:
+    print(f" Connection error: {e}")
+    raise
