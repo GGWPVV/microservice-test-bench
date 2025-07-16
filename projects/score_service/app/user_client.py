d479d7c4a4159fc6cd91cb5f16fe5843be427ad8
GGWPVV <gvlportugal95@gmail.com>
2025-07-17 00:17:52 +0100

    fix: from user-service to user_service

--- projects/score_service/app/user_client.py
+++ projects/score_service/app/user_client.py
@@ -1,10 +1,11 @@
+import requests
 def get_user(token: str):
     if not token.startswith("Bearer "):
         token = f"Bearer {token}"  # Ensure the token starts with "Bearer "
 
     headers = {"Authorization": token}
     try:
-        response = requests.get("http://user-service:8000/user/me", headers=headers)
+        response = requests.get("http://user_service:8000/user/me", headers=headers)
         if response.status_code == 200:
             return response.json()
         print("User service response:", response.status_code, response.text)
