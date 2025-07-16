f8c784eb00f30b8dfc3b03efe88b5f53f13ec8f2
GGWPVV <gvlportugal95@gmail.com>
2025-07-17 00:20:46 +0100

    add: sername to user_score table

--- projects/score_service/app/models.py
+++ projects/score_service/app/models.py
@@ -13,3 +13,4 @@ class UserScore(Base):
     user_id = Column(UUID(as_uuid=True), nullable=False) 
     score = Column(Integer, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+    username = Column(String, index=True)
