e481ef774c9552ab9a2ec6b36d826495a8a8527d
GGWPVV <gvlportugal95@gmail.com>
2025-07-16 23:20:46 +0100

    fix: token check

--- projects/score_service/app/main.py
+++ projects/score_service/app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Depends, HTTPException, Header
 from sqlalchemy.orm import Session
-from uuid import UUID
 import random
+from datetime import datetime
+
 import models, database
 from user_client import get_user
 from datetime import datetime
@@ -17,8 +18,11 @@ def get_db():
         db.close()
 
 @app.post("/roll")
-def draw_score(Authorization: str = Header(...), db: Session = Depends(get_db)):
-    # Авторизация и извлечение user_id из JWT
+def draw_score(
+    Authorization: str = Header(...), 
+    db: Session = Depends(get_db)
+):
+    # 1. Проверка токена и получение user_id, username
     user_data = get_user(Authorization)
     if not user_data:
         raise HTTPException(status_code=403, detail="Invalid token or user not found")
@@ -26,12 +30,12 @@ def draw_score(Authorization: str = Header(...), db: Session = Depends(get_db)):
     user_id = user_data["id"]
     username = user_data["username"]
 
-    # Проверка, крутил ли уже рулетку
+    # 2. Проверка, не играл ли уже пользователь
     existing_score = db.query(models.UserScore).filter_by(user_id=user_id).first()
     if existing_score:
         raise HTTPException(status_code=400, detail="Ты уже использовал рулетку")
 
-    # Генерация числа и запись
+    # 3. Генерация и запись
     score_value = random.randint(1, 1_000_000)
     new_score = models.UserScore(
         user_id=user_id,
@@ -41,6 +45,7 @@ def draw_score(Authorization: str = Header(...), db: Session = Depends(get_db)):
     )
     db.add(new_score)
     db.commit()
+
     return {
         "username": username,
         "score": score_value,
