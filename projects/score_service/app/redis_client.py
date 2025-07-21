9506edc3a410aad9b023477275259af70b614638
GGWPVV <gvlportugal95@gmail.com>
2025-07-21 19:04:22 +0100

    fix: sync to async

--- projects/score_service/app/redis_client.py
+++ projects/score_service/app/redis_client.py
@@ -1,24 +1,30 @@
-import redis
+# redis_client.py
+
 import os
 import json
+import redis.asyncio as redis 
 
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 
-r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 LEADERBOARD_CACHE_KEY = "top_10_users"
-ROLL_CACHE_PREFIX = "roll_done:"  # по user_id
+ROLL_CACHE_PREFIX = "roll_done:"  
 
-def cache_leaderboard(data: list):
-    r.set(LEADERBOARD_CACHE_KEY, json.dumps(data), ex=60)   # TTL 60 seconds
 
-def get_cached_leaderboard():
-    raw = r.get(LEADERBOARD_CACHE_KEY)
+async def get_redis():
+    return redis_client
+
+async def cache_leaderboard(data: list):
+    await redis_client.set(LEADERBOARD_CACHE_KEY, json.dumps(data), ex=60) 
+
+async def get_cached_leaderboard():
+    raw = await redis_client.get(LEADERBOARD_CACHE_KEY)
     return json.loads(raw) if raw else None
 
-def mark_user_rolled(user_id: str):
-    r.set(f"{ROLL_CACHE_PREFIX}{user_id}", "1")
+async def mark_user_rolled(user_id: str):
+    await redis_client.set(f"{ROLL_CACHE_PREFIX}{user_id}", "1")
 
-def has_user_rolled(user_id: str) -> bool:
-    return r.exists(f"{ROLL_CACHE_PREFIX}{user_id}") == 1
+async def has_user_rolled(user_id: str) -> bool:
+    return await redis_client.exists(f"{ROLL_CACHE_PREFIX}{user_id}") == 1
